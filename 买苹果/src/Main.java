e5ea17e40669a99e46fc7c9ab8cce5cac92f0907
鹤 <62049028+matilda-art@users.noreply.github.com>
2020-06-04 11:45:32 +0800

    数学思想
    数学思想

--- 买苹果/src/Main.java
+++ 买苹果/src/Main.java
@@ -9,6 +9,12 @@ import java.util.Scanner;
  * @author: matilda
  * @create: 2020-06-04 10:31
  **/
+
+//思路：当n<5时，不能恰好购买，将不会购买，返回-1
+//      当n/8余数为0时，选用n/8个袋子进行携带
+//      当n/8的余数是偶数时，可以拆分成6的倍数进行计算（n=10的情况不满足）
+//      例如：18/8 = 2……2；可拆分成：18/6 = 3；
+//            结果为n/8 + 1
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
