53ab3b4ec2eed4734acfaa535f20f9e7cb4f6734
matilda-art <heysober@163.com>
2020-06-21 22:18:03 +0800

    使用contains

---
+++ 子串判断/src/Substr.java
@@ -0,0 +1,21 @@
+/**
+ * @program: 子串判断
+ * @description
+ * 现有一个小写英文字母组成的字符串s和一个包含较短小写英文字符串的数组p
+ * 请设计一个高效算法，对于p中的每一个较短字符串，判断其是否为s的子串。
+ * 给定一个string数组p和它的大小n，同时给定string s，为母串
+ * 请返回一个bool数组，每个元素代表p中的对应字符串是否为s的子串。
+ * 保证p中的串长度小于等于8，且p中的串的个数小于等于500，同时保证s的长度小于等于1000
+ * @author: matilda
+ * @create: 2020-06-21 21:10
+ **/
+public class Substr {
+    public boolean[] chkSubStr(String[] p, int n, String s) {
+        boolean[] hasSub = new boolean[p.length];
+        for (int i = 0; i < p.length; i++) {
+            //因为s.contains本来返回的就是Boolean类型的值，所以直接赋值
+            hasSub[i] = s.contains(p[i]);
+        }
+        return hasSub;
+    }
+}
