24c558239f7948421e0cf107fa3230dd0bd0dcf1
matilda-art <heysober@163.com>
2020-07-08 22:07:19 +0800

    锤子剪刀布

---
+++ 锤子剪刀布/src/Main.java
@@ -0,0 +1,113 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * @program: 锤子剪刀布
+ * @description
+ * 大家应该都会玩“锤子剪刀布”的游戏：
+ * 现给出两人的交锋记录，请统计双方的胜、平、负次数，并且给出双方分别出什么手势胜算最大
+ *
+ * 输入描述：
+ * 输入第1行给出正整数N(<=105)，即双方交锋的次数。随后N行，每行给出一次交锋的信息，
+ * 即甲、乙双方同时给出的手势。
+ * C代表“锤子”、J代表“剪刀”、B代表“布”，第1个字母代表甲方，第2个代表乙方，中间有1个空格。
+ *
+ * 输出描述：
+ * 输出第1、2行分别给出甲、乙的胜、平、负次数，数字间以1个空格分隔。
+ * 第3行给出两个字母，分别代表甲、乙获胜次数最多的手势，中间有1个空格。
+ * 如果解不唯一，则输出按字母序最小的解。
+ * @author: matilda
+ * @create: 2020-07-08 20:53
+ **/
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        Mode1 mA = new Mode1();
+        Mode1 mB = new Mode1();
+
+        for (int i = 0; i < N; i++) {
+            String a = scanner.next();
+            String b = scanner.next();
+            judge(a,b,mA,mB);
+        }
+
+        System.out.println(mA.win+" "+mA.tie+" "+mA.lose);
+        System.out.println(mB.win+" "+mB.tie+" "+mB.lose);
+        System.out.println(getMostGen(mA.map)+" "+getMostGen(mB.map));
+    }
+
+    public static void judge(String a,String b,Mode1 mA,Mode1 mB){
+        if (a.equals("C")){
+            if (b.equals("C")){
+                mA.tie++;
+                mB.tie++;
+            }else if (b.equals("J")){
+                mA.win++;
+                mB.lose++;
+                mA.map.put("C",mA.map.get("C")+1);
+            }else {
+                mA.lose++;
+                mB.win++;
+                mB.map.put("B",mA.map.get("B")+1);
+            }
+        }else if (a.equals("J")){
+            if (b.equals("C")){
+                mA.lose++;
+                mB.win++;
+                mB.map.put("C",mA.map.get("C")+1);
+            }else if (b.equals("J")){
+                mA.tie++;
+                mB.tie++;
+            }else {
+                mA.win++;
+                mB.lose++;
+                mA.map.put("J",mA.map.get("J")+1);
+            }
+        }else {
+            if (b.equals("C")){
+                mA.win++;
+                mB.lose++;
+                mA.map.put("B",mA.map.get("B")+1);
+            }else if (b.equals("J")){
+                mA.lose++;
+                mB.win++;
+                mB.map.put("J",mA.map.get("J")+1);
+            }else {
+                mA.tie++;
+                mB.tie++;
+            }
+        }
+    }
+
+    public static String getMostGen(Map<String,Integer> map){
+        if (map.get("C") >= map.get("J")){
+            if (map.get("C") > map.get("B")){
+                return "C";
+            }else {
+                return "B";
+            }
+        }else {
+            if (map.get("J") > map.get("B")){
+                return "J";
+            }else {
+                return "B";
+            }
+        }
+    }
+
+    public static class Mode1{
+        int win;
+        int tie;
+        int lose;
+        Map<String,Integer> map = new HashMap<>();
+
+        Mode1(){
+            map.put("B",0);
+            map.put("C",0);
+            map.put("J",0);
+        }
+    }
+}
